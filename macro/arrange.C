7f5727533b37e959719c7f5de7302deb3f611219
Somhammer <klar.wind0425@gmail.com>
2018-11-22 18:27:45 +0900

    change variables name

--- macro/arrange.C
+++ macro/arrange.C
@@ -14,17 +14,35 @@ using namespace std;
 
 string input = "/home/seohyun/work/ttbb/heptool/tmp/";
 string output = "/home/seohyun/work/ttbb/heptool/output/root/";
-string ntuple = "/data/users/seohyun/ntuple/hep2017/v808/nosplit/";
 
-void merge(){
-  vector<string> v_histName;
-  for(int i=0;i<2;++i){
-    v_histName.push_back(Form("h_nJets_Ch%d_S3_",i));
-    v_histName.push_back(Form("h_nBjets_Ch%d_S3_",i));
-    v_histName.push_back(Form("h_RecoJetDeltaR_Ch%d_S3_",i));
-    v_histName.push_back(Form("h_RecoJetInvMass_Ch%d_S3_",i));
+std::string ReplaceAll(std::string &str, const std::string &from, const std::string &to){
+  size_t start_pos = 0;
+  while((start_pos = str.find(from, start_pos)) != std::string::npos){
+    str.replace(start_pos, from.length(), to);
+    start_pos += to.length();
   }
+  return str;
+}
+
+void arrange(){
+  TFile *f_filter = TFile::Open(Form("%s/hist_TTLJ_PowhegPythia_ttbbFilter_ttbb.root",input.c_str()));
+
+  vector<string> v_histName;
+
+  TIter next(f_filter->GetListOfKeys());
+  TKey *key;
+  TObject *obj;
+  while((key = (TKey *)next())){
+    obj = key->ReadObj();
+    string tmp = obj->GetName();
+    ReplaceAll(tmp, "TTLJ_PowhegPythia_ttbbFilter_ttbb","");
+    v_histName.push_back(tmp);
+    cout << tmp << endl;
+  }
+
   const int nHist = v_histName.size();
+  cout << "Number of histograms: " << nHist << endl;
+
   vector<string> v_dataName_mu = {
     "DataSingleMuB","DataSingleMuC","DataSingleMuD","DataSingleMuE","DataSingleMuF",
     "DataSingleMuG","DataSingleMuHv2","DataSingleMuHv3"
@@ -46,6 +64,12 @@ void merge(){
   f_out_mu->cd();
   TH1 *h_mu[nHist];
   for( int i=0; i<nHist; ++i){
+    ssize_t pos;
+    if(((pos = (v_histName.at(i)).find("Gen",0,3)) != std::string::npos)
+	|| ((pos = (v_histName.at(i)).find("Resp",0,4)) != std::string::npos))
+      continue;
+    if((pos = (v_histName.at(i)).find("EventInfo",0,5)) != std::string::npos)
+      continue;
     THStack *hs_tmp = new THStack("st","");
     for(int j=0; j<nData; ++j){
       string histName = v_histName.at(i)+v_dataName_mu[j];
@@ -64,6 +88,12 @@ void merge(){
   f_out_el->cd();
   TH1 *h_el[nHist];
   for( int i=0; i<nHist; ++i){
+    ssize_t pos;
+    if(((pos = (v_histName.at(i)).find("Gen",0,3)) != std::string::npos)
+	|| ((pos = (v_histName.at(i)).find("Resp",0,4)) != std::string::npos))
+      continue;
+    if((pos = (v_histName.at(i)).find("EventInfo",0,5)) != std::string::npos)
+      continue;
     THStack *hs_tmp = new THStack("st","");
     for(int j=0; j<nData; ++j){
       string histName = v_histName.at(i)+v_dataName_eg[j];
@@ -82,7 +112,7 @@ void merge(){
     f_mu[i]->Close();
     f_el[i]->Close();
   }
-
+  
   vector<string> v_sampleName = {
     "tbar_Powheg","tbarW_Powheg","t_Powheg","tW_Powheg","TTLJ_PowhegPythia_ttLF","TTLJ_PowhegPythia_ttbb","TTLJ_PowhegPythia_ttbj","TTLJ_PowhegPythia_ttcc","TTLJ_PowhegPythia_ttjj","TTLJ_PowhegPythia_ttother","ttHbb_PowhegPythia","ttW_Madgraph","ttZ_Madgraph","WJets_aMCatNLO","ZJets_M10to50_aMCatNLO","ZJets_M50_aMCatNLO","WW_Pythia","WZ_Pythia","ZZ_Pythia","ttbar_PowhegPythiaBkg"
   };
@@ -96,42 +126,74 @@ void merge(){
   for(int i=0;i<20;++i){
     f_in[i] = TFile::Open(Form("%s/hist_%s.root",input.c_str(),v_sampleName[i].c_str()));
     f_out[i] = TFile::Open(Form("%s/hist_%s.root",output.c_str(),v_sampleName_out[i].c_str()),"recreate");
-    f_tree[i] = TFile::Open(Form("%s/%s.root",ntuple.c_str(),v_sampleName[i].c_str()));
   }
 
-  
   for(int i=0; i<nSample; ++i){
     f_out[i]->cd();
     for(int j=0; j<nHist; ++j){
+      ssize_t pos;
+      if(((pos = (v_histName.at(j)).find("Gen",0,3)) != std::string::npos)
+	  || ((pos = (v_histName.at(j)).find("Resp",0,4)) != std::string::npos))
+        continue;
+      if((pos = (v_histName.at(j)).find("EventInfo",0,5)) != std::string::npos)
+        continue;
       string histName = v_histName.at(j)+v_sampleName.at(i);
       auto h_tmp = (TH1 *)f_in[i]->Get(histName.c_str());
       string histName2 = v_histName.at(j)+v_sampleName_out.at(i);
       h_tmp->SetName(histName2.c_str());
       h_tmp->Write();
     }
-    TH1 *eventInfo = (TH1 *)f_tree[i]->Get("ttbbLepJets/EventInfo");
+    TH1 *eventInfo = (TH1 *)f_in[i]->Get("EventInfo");
     eventInfo->Write();
     f_out[i]->Write();
     f_out[i]->Close();
-    f_tree[i]->Close();
   }
 
   TFile *f_matrix = TFile::Open(Form("%s/hist_respMatrix_ttbb.root",output.c_str()),"recreate");
-
-  vector<string> v_histNameMatrix;
-  for(int i=0; i<2; ++i){
-    v_histNameMatrix.push_back(Form("h_GenbJetDeltaR_Ch%d_nosel_TTLJ_PowhegPythia_ttbb",i));
-    v_histNameMatrix.push_back(Form("h_GenbJetInvMass_Ch%d_nosel_TTLJ_PowhegPythia_ttbb",i));
-    v_histNameMatrix.push_back(Form("h_GenbJetDeltaR_Ch%d_S3_TTLJ_PowhegPythia_ttbb",i));
-    v_histNameMatrix.push_back(Form("h_GenbJetInvMass_Ch%d_S3_TTLJ_PowhegPythia_ttbb",i));
-    v_histNameMatrix.push_back(Form("h_ResponseMatrixDeltaR_Ch%d_S3_TTLJ_PowhegPythia_ttbb",i));
-    v_histNameMatrix.push_back(Form("h_ResponseMatrixInvMass_Ch%d_S3_TTLJ_PowhegPythia_ttbb",i));
-  }
   f_matrix->cd();
-  for(unsigned i=0; i<v_histNameMatrix.size();++i){
-    auto h_tmp = (TH1 *)f_in[5]->Get((v_histNameMatrix.at(i)).c_str());
-    h_tmp->Write();
+
+  for(int i=0; i<2; ++i){
+    auto h1 = (TH1 *)f_filter->Get(Form("h_ResponseMatrixDeltaR_Ch%d_S3_TTLJ_PowhegPythia_ttbbFilter_ttbb",i));
+    auto h2 = (TH1 *)f_filter->Get(Form("h_ResponseMatrixInvMass_Ch%d_S3_TTLJ_PowhegPythia_ttbbFilter_ttbb",i));
+    h1->Write();
+    h2->Write();
   }
+
+  TH1 *info = (TH1 *)f_filter->Get("EventInfo");
+  info->Write();
   f_matrix->Write();
   f_matrix->Close();
+
+  vector<string> v_systematics = {
+    "TT_PowhegPythia_SYS_FSRup", "TT_PowhegPythia_SYS_FSRdown",
+    "TT_PowhegPythia_SYS_ISRup", "TT_PowhegPythia_SYS_ISRdown",
+    "TT_PowhegHerwig", "TT_PowhegPythiaEvtgen", "TT_aMCatNLOPythia",
+    "TTLJ_PowhegPythia"
+  };
+  
+  const int nsys = v_systematics.size();
+  TFile *f_sysin[nsys], *f_sysout[nsys];
+  for(int i=0; i < nsys; ++i){
+    f_sysin[i] = TFile::Open(Form("%s/hist_%s_ttbb.root", input.c_str(), v_systematics[i].c_str()));
+    f_sysout[i] = TFile::Open(Form("%s/hist_%s_ttbb.root", output.c_str(),v_systematics[i].c_str()),"recreate");
+  }
+  
+  for(int i=0; i<nsys; ++i){
+    f_sysout[i]->cd();
+    cout << v_systematics.at(i) << endl;
+    ssize_t pos;
+    for(int j=0; j<nHist; ++j){
+      if((pos = (v_histName.at(j)).find("EventInfo",0,5)) != std::string::npos)
+        continue;
+      string histName = v_histName.at(j)+v_systematics.at(i)+"_ttbb";
+      auto h_tmp = (TH1 *)f_sysin[i]->Get(histName.c_str());
+      string histName2 = v_histName.at(j)+"ttbb";
+      h_tmp->SetName(histName2.c_str());
+      h_tmp->Write();
+    }
+    TH1 *eventInfo = (TH1 *)f_sysin[i]->Get("EventInfo");
+    eventInfo->Write();
+    f_sysout[i]->Write();
+    f_sysout[i]->Close();
+  }
 }
--- macro/makeCriteria.C
+++ macro/makeCriteria.C
@@ -1,22 +1,54 @@
 #include "../include/histBook.h"
 #include "../include/tdrstyle.C"
 
-void drawHist(TH1 *h_in, bool drawError);
+TFile *MakeRoot(string name);
+void DrawHist(TH1 *h_in, std::string name, bool drawError);
 
 string input = "../output/root/";
 string output = "../output/root/";
 string output_pdf = "../output/pdf/";
 
-void makeStability(){
-  //FILES
-  TFile *f_ttbb = TFile::Open("/data/users/seohyun/ntuple/hep2017/v808/nosplit/TTLJ_PowhegPythia_ttbb.root");
-  TFile *f_matrix = TFile::Open(Form("%shist_respMatrix_ttbb.root",input.c_str()));
-  TFile *f_outFile = TFile::Open(Form("%shist_stability_ttbb.root",output.c_str()),"recreate");
-  
+void makeCriteria(){
+  std::vector<std::string> v_name = {
+    "TTLJ_PowhegPythia", "TT_PowhegHerwig", "TT_PowhegPythiaEvtgen", "TT_aMCatNLOPythia",
+    "TT_PowhegPythia_SYS_FSRup", "TT_PowhegPythia_SYS_FSRdown", "TT_PowhegPythia_SYS_ISRup", "TT_PowhegPythia_SYS_ISRdown"
+  };
+
+  for(int i=0; i < v_name.size(); ++i){
+    auto f_tmp = MakeRoot(v_name.at(i));
+    TIter next(f_tmp->GetListOfKeys());
+    TKey *key;
+    TObject *obj;
+    while((key = (TKey *)next())){
+      obj = key->ReadObj();
+      string tmp = obj->GetName();
+      ssize_t pos;
+      if((pos = tmp.find("Bin",0,3)) == std::string::npos)
+	  continue;
+      auto h_tmp = (TH1 *)obj;
+      if((pos = tmp.find("Acc",0,3)) != std::string::npos)
+	DrawHist(h_tmp, v_name.at(i), true);
+      else
+        DrawHist(h_tmp, v_name.at(i), false);
+    }
+  }
+}
+
+TFile *MakeRoot(string name_){
+  auto name = name_;
+  cout << "SAMPLE: " << name << endl;
+
+  TFile *f_in = TFile::Open(Form("%s/hist_%s_ttbb.root", input.c_str(), name.c_str()));
+  TFile *f_out = TFile::Open(Form("%s/hist_criteria_%s_ttbb.root",output.c_str(),name.c_str()),"recreate");
+
   //NORMALIZATION
-  TH1 *EventInfo = (TH1 *)f_ttbb->Get("ttbbLepJets/EventInfo");
-  const double scale = LUMINOSITY_*XSEC_[TTBB_]/EventInfo->GetBinContent(2);
-  //const double scale = LUMINOSITY_*831.76/EventInfo->GetBinContent(2);
+  TH1 *EventInfo = (TH1 *)f_in->Get("EventInfo");
+  double scale;
+  ssize_t pos;
+  if((pos = name.find("TTLJ",0,4)) != std::string::npos)
+    scale = LUMINOSITY_*XSEC_[TTBB_]/EventInfo->GetBinContent(2);
+  else 
+    scale = LUMINOSITY_*831.76/EventInfo->GetBinContent(2);
 
   //HISTOGRAMS
   HistoBook *h_stb = new HistoBook(3,NAME_[TTBB_].c_str());
@@ -36,18 +68,24 @@ void makeStability(){
   TH1D *h_gen_addbjets_afterSel_invMass[nChannel];
 
   for(int iChannel=0; iChannel<nChannel; ++iChannel){    
-    h_gen_addbjets_nosel_deltaR[iChannel] = (TH1D *)f_matrix->Get(Form("h_GenbJetDeltaR_Ch%d_nosel_TTLJ_PowhegPythia_ttbb",iChannel));
+    h_gen_addbjets_nosel_deltaR[iChannel] = (TH1D *)f_in->Get(Form("h_%s_Ch%d_nosel_ttbb",
+	  GEN_ADDBJETS_DELTAR_, iChannel));
     h_gen_addbjets_nosel_deltaR[iChannel]->Scale(scale);
-    h_gen_addbjets_nosel_invMass[iChannel] = (TH1D *)f_matrix->Get(Form("h_GenbJetInvMass_Ch%d_nosel_TTLJ_PowhegPythia_%s",iChannel,NAME_[TTBB_].c_str()));
+    h_gen_addbjets_nosel_invMass[iChannel] = (TH1D *)f_in->Get(Form("h_%s_Ch%d_nosel_ttbb",
+	  GEN_ADDBJETS_INVARIANT_MASS_, iChannel));
     h_gen_addbjets_nosel_invMass[iChannel]->Scale(scale);
-    h_gen_addbjets_afterSel_deltaR[iChannel] = (TH1D *)f_matrix->Get(Form("h_GenbJetDeltaR_Ch%d_S3_TTLJ_PowhegPythia_%s",iChannel,NAME_[TTBB_].c_str()));
+    h_gen_addbjets_afterSel_deltaR[iChannel] = (TH1D *)f_in->Get(Form("h_%s_Ch%d_S3_ttbb",
+	  GEN_ADDBJETS_DELTAR_, iChannel));
     h_gen_addbjets_afterSel_deltaR[iChannel]->Scale(scale);
-    h_gen_addbjets_afterSel_invMass[iChannel] = (TH1D *)f_matrix->Get(Form("h_GenbJetInvMass_Ch%d_S3_TTLJ_PowhegPythia_%s",iChannel,NAME_[TTBB_].c_str()));
+    h_gen_addbjets_afterSel_invMass[iChannel] = (TH1D *)f_in->Get(Form("h_%s_Ch%d_S3_ttbb",
+	  GEN_ADDBJETS_INVARIANT_MASS_, iChannel));
     h_gen_addbjets_afterSel_invMass[iChannel]->Scale(scale);
-
-    h_respMatrix_deltaR[iChannel] = (TH2D *)f_matrix->Get(Form("h_%s_Ch%d_S3_TTLJ_PowhegPythia_%s",RESPONSE_MATRIX_DELTAR_,iChannel,NAME_[TTBB_].c_str()));
+    
+    h_respMatrix_deltaR[iChannel] = (TH2D *)f_in->Get(Form("h_%s_Ch%d_S3_ttbb",
+	  RESPONSE_MATRIX_DELTAR_,iChannel));
     h_respMatrix_deltaR[iChannel]->Scale(scale);
-    h_respMatrix_invMass[iChannel] = (TH2D *)f_matrix->Get(Form("h_%s_Ch%d_S3_TTLJ_PowhegPythia_%s",RESPONSE_MATRIX_INVARIANT_MASS_,iChannel,NAME_[TTBB_].c_str()));
+    h_respMatrix_invMass[iChannel] = (TH2D *)f_in->Get(Form("h_%s_Ch%d_S3_ttbb",
+	  RESPONSE_MATRIX_INVARIANT_MASS_,iChannel));
     h_respMatrix_invMass[iChannel]->Scale(scale);
 
     h_gen_addbjets_deltaR[iChannel] = h_respMatrix_deltaR[iChannel]->ProjectionY();
@@ -56,10 +94,11 @@ void makeStability(){
     h_reco_addjets_deltaR[iChannel] = h_respMatrix_deltaR[iChannel]->ProjectionX();
     h_reco_addjets_invMass[iChannel] = h_respMatrix_invMass[iChannel]->ProjectionX();
   }
- 
+
   for(int iChannel=0; iChannel<nChannel; ++iChannel){
     cout << "=======================================" << endl;
     cout << "Channel : " << iChannel << endl;
+    
     cout << "----------------------------------------" << endl;
     cout << "Variable : delta R" << endl;
     cout << "********** Purity **********" << endl;
@@ -87,20 +126,20 @@ void makeStability(){
       cout << iBin << "th Bin" << endl;
       double stability_dR = 0.0;
       if(h_gen_addbjets_deltaR[iChannel]->GetBinContent(iBin) != 0.0){
-        auto ylow = h_respMatrix_deltaR[iChannel]->GetYaxis()->GetBinLowEdge(iBin);
-        auto ymax = h_respMatrix_deltaR[iChannel]->GetYaxis()->GetBinLowEdge(iBin+1);
-        auto xlow = h_respMatrix_deltaR[iChannel]->GetXaxis()->FindBin(ylow);
-        auto xmax = h_respMatrix_deltaR[iChannel]->GetXaxis()->FindBin(ymax);
-        if(xlow == xmax && xmax == h_respMatrix_deltaR[iChannel]->GetNbinsX()) ++xmax;
-        double xbincontent = 0.;
-        for(int ixbin = xlow; ixbin < xmax; ++ixbin)
+	auto ylow = h_respMatrix_deltaR[iChannel]->GetYaxis()->GetBinLowEdge(iBin);
+	auto ymax = h_respMatrix_deltaR[iChannel]->GetYaxis()->GetBinLowEdge(iBin+1);
+	auto xlow = h_respMatrix_deltaR[iChannel]->GetXaxis()->FindBin(ylow);
+	auto xmax = h_respMatrix_deltaR[iChannel]->GetXaxis()->FindBin(ymax);
+	if(xlow == xmax && xmax == h_respMatrix_deltaR[iChannel]->GetNbinsX()) ++xmax;
+	double xbincontent = 0.;
+	for(int ixbin = xlow; ixbin < xmax; ++ixbin)
 	  xbincontent += h_respMatrix_deltaR[iChannel]->GetBinContent(ixbin,iBin);
-        stability_dR = xbincontent/h_gen_addbjets_deltaR[iChannel]->GetBinContent(iBin);
-        cout << "y low : " << ylow << ", max : " << ymax << endl;
-        cout << "x low : " << xlow << ", max : " << xmax << endl;
-        cout << "xBinContent : " << xbincontent << endl;
-        cout << "yBinContent : " << h_gen_addbjets_deltaR[iChannel]->GetBinContent(iBin) << endl;
-        cout << "value :" << stability_dR << endl;
+	stability_dR = xbincontent/h_gen_addbjets_deltaR[iChannel]->GetBinContent(iBin);
+	cout << "y low : " << ylow << ", max : " << ymax << endl;
+	cout << "x low : " << xlow << ", max : " << xmax << endl;
+	cout << "xBinContent : " << xbincontent << endl;
+	cout << "yBinContent : " << h_gen_addbjets_deltaR[iChannel]->GetBinContent(iBin) << endl;
+	cout << "value :" << stability_dR << endl;
       }
       h_stb->h_stability_deltaR[iChannel]->SetBinContent(iBin, stability_dR);
     }
@@ -113,15 +152,15 @@ void makeStability(){
       cout << iBin << "th Bin" << endl;
       double purity_M = 0.0;
       if(h_reco_addjets_invMass[iChannel]->GetBinContent(iBin) != 0.0){
-        auto xlow = h_respMatrix_invMass[iChannel]->GetXaxis()->GetBinLowEdge(iBin);
-        auto ybin = h_respMatrix_invMass[iChannel]->GetYaxis()->FindBin(xlow);
-        purity_M = h_respMatrix_invMass[iChannel]->GetBinContent(iBin,ybin)/h_reco_addjets_invMass[iChannel]->GetBinContent(iBin);
+	auto xlow = h_respMatrix_invMass[iChannel]->GetXaxis()->GetBinLowEdge(iBin);
+	auto ybin = h_respMatrix_invMass[iChannel]->GetYaxis()->FindBin(xlow);
+	purity_M = h_respMatrix_invMass[iChannel]->GetBinContent(iBin,ybin)/h_reco_addjets_invMass[iChannel]->GetBinContent(iBin);
       
-        cout << "x low : " << xlow << endl;
-        cout << "y bin number : " << ybin << endl;
-        cout << "xBinContent : " << h_reco_addjets_invMass[iChannel]->GetBinContent(iBin) << endl;
-        cout << "yBinContent : " << h_respMatrix_invMass[iChannel]->GetBinContent(iBin,ybin) << endl;
-        cout << "value : " << purity_M << endl;
+	cout << "x low : " << xlow << endl;
+	cout << "y bin number : " << ybin << endl;
+	cout << "xBinContent : " << h_reco_addjets_invMass[iChannel]->GetBinContent(iBin) << endl;
+	cout << "yBinContent : " << h_respMatrix_invMass[iChannel]->GetBinContent(iBin,ybin) << endl;
+	cout << "value : " << purity_M << endl;
       }
       h_stb->h_purity_invMass[iChannel]->SetBinContent(iBin, purity_M);
     }
@@ -132,140 +171,128 @@ void makeStability(){
       cout << iBin << "th Bin" << endl;
       double stability_M = 0.0;
       if(h_gen_addbjets_invMass[iChannel]->GetBinContent(iBin) != 0.0){
-        auto ylow = h_respMatrix_invMass[iChannel]->GetYaxis()->GetBinLowEdge(iBin);
-        auto ymax = h_respMatrix_invMass[iChannel]->GetYaxis()->GetBinLowEdge(iBin+1);
-        auto xlow = h_respMatrix_invMass[iChannel]->GetXaxis()->FindBin(ylow);
-        auto xmax = h_respMatrix_invMass[iChannel]->GetXaxis()->FindBin(ymax);
-        if(xlow == xmax && xmax == h_respMatrix_invMass[iChannel]->GetNbinsX()) ++xmax;
-        double xbincontent = 0.;
-        for(int ixbin = xlow; ixbin < xmax; ++ixbin)
+	auto ylow = h_respMatrix_invMass[iChannel]->GetYaxis()->GetBinLowEdge(iBin);
+	auto ymax = h_respMatrix_invMass[iChannel]->GetYaxis()->GetBinLowEdge(iBin+1);
+	auto xlow = h_respMatrix_invMass[iChannel]->GetXaxis()->FindBin(ylow);
+	auto xmax = h_respMatrix_invMass[iChannel]->GetXaxis()->FindBin(ymax);
+	if(xlow == xmax && xmax == h_respMatrix_invMass[iChannel]->GetNbinsX()) ++xmax;
+	double xbincontent = 0.;
+	for(int ixbin = xlow; ixbin < xmax; ++ixbin)
 	  xbincontent += h_respMatrix_invMass[iChannel]->GetBinContent(ixbin,iBin);
-        stability_M = xbincontent/h_gen_addbjets_invMass[iChannel]->GetBinContent(iBin);
+	stability_M = xbincontent/h_gen_addbjets_invMass[iChannel]->GetBinContent(iBin);
       
-        cout << "y low : " << ylow << ", max : " << ymax << endl;
-        cout << "x low : " << xlow << ", max : " << xmax << endl;
-        cout << "xBinContent : " << xbincontent << endl;
-        cout << "yBinContent : " << h_gen_addbjets_invMass[iChannel]->GetBinContent(iBin) << endl;
-        cout << "value :" << stability_M << endl;
+	cout << "y low : " << ylow << ", max : " << ymax << endl;
+	cout << "x low : " << xlow << ", max : " << xmax << endl;
+	cout << "xBinContent : " << xbincontent << endl;
+	cout << "yBinContent : " << h_gen_addbjets_invMass[iChannel]->GetBinContent(iBin) << endl;
+	cout << "value :" << stability_M << endl;
       }
       h_stb->h_stability_invMass[iChannel]->SetBinContent(iBin, stability_M);
     }
+    
     cout << "----------------------------------------" << endl;
 
     //ACCEPTANCE
+    cout << "----------------------------------------" << endl;
+    cout << "********** Acceptance  **********" << endl;
+    cout << "Variable : delta R" << endl;
     for(int iBin=1; iBin<h_stb->xNbins_gen_addbjets_dR+1;++iBin){
+      cout << iBin << "th Bin" << endl;
       double acceptance_dR = 0.0;
       double dR_err = 0.0;
       if( h_gen_addbjets_nosel_deltaR[iChannel]->GetBinContent(iBin) != 0.0 ){
-        acceptance_dR = h_gen_addbjets_afterSel_deltaR[iChannel]->GetBinContent(iBin)/(h_gen_addbjets_nosel_deltaR[0]->GetBinContent(iBin)+h_gen_addbjets_nosel_deltaR[1]->GetBinContent(iBin));
+	acceptance_dR = h_gen_addbjets_afterSel_deltaR[iChannel]->GetBinContent(iBin)/(h_gen_addbjets_nosel_deltaR[0]->GetBinContent(iBin)+h_gen_addbjets_nosel_deltaR[1]->GetBinContent(iBin));
 	dR_err = abs(acceptance_dR)*sqrt(pow(h_gen_addbjets_afterSel_deltaR[iChannel]->GetBinError(iBin)/h_gen_addbjets_afterSel_deltaR[iChannel]->GetBinContent(iBin),2)
 	    +pow(h_gen_addbjets_nosel_deltaR[0]->GetBinError(iBin)/h_gen_addbjets_nosel_deltaR[0]->GetBinContent(iBin),2)+pow(h_gen_addbjets_nosel_deltaR[0]->GetBinError(iBin)/h_gen_addbjets_nosel_deltaR[0]->GetBinContent(iBin),2));
       }
       h_stb->h_acceptance_deltaR[iChannel]->SetBinContent(iBin, acceptance_dR);
       h_stb->h_acceptance_deltaR[iChannel]->SetBinError(iBin, dR_err);
+      cout << "value :" << acceptance_dR << endl;
+      cout << "err :" << dR_err << endl;
     }
     
+    cout << "Variable : invariant mass" << endl;
     for(int iBin=1; iBin<h_stb->xNbins_gen_addbjets_M+1;++iBin){
+      cout << iBin << "th Bin" << endl;
       double acceptance_M = 0.0;
       double M_err = 0.0;
       if( h_gen_addbjets_nosel_invMass[iChannel]->GetBinContent(iBin) != 0.0 ){
-        acceptance_M = h_gen_addbjets_afterSel_invMass[iChannel]->GetBinContent(iBin)/(h_gen_addbjets_nosel_invMass[0]->GetBinContent(iBin)+h_gen_addbjets_nosel_invMass[1]->GetBinContent(iBin));
+	acceptance_M = h_gen_addbjets_afterSel_invMass[iChannel]->GetBinContent(iBin)/(h_gen_addbjets_nosel_invMass[0]->GetBinContent(iBin)+h_gen_addbjets_nosel_invMass[1]->GetBinContent(iBin));
 	M_err = abs(acceptance_M)*sqrt(pow(h_gen_addbjets_afterSel_invMass[iChannel]->GetBinError(iBin)/h_gen_addbjets_afterSel_invMass[iChannel]->GetBinContent(iBin),2)
 	    +pow(h_gen_addbjets_nosel_invMass[0]->GetBinError(iBin)/h_gen_addbjets_nosel_invMass[0]->GetBinContent(iBin),2)+pow(h_gen_addbjets_nosel_invMass[1]->GetBinError(iBin)/h_gen_addbjets_nosel_invMass[1]->GetBinContent(iBin),2));
       }
       h_stb->h_acceptance_invMass[iChannel]->SetBinContent(iBin, acceptance_M);
       h_stb->h_acceptance_invMass[iChannel]->SetBinError(iBin, M_err);
+      cout << "value :" << acceptance_M << endl;
+      cout << "err :" << M_err << endl;
+
     }
   }//Channel
-  f_outFile->Write();
-  
+  f_out->Write();
+
+  return f_out;
+}
+
+void DrawHist(TH1* h_in_, std::string name, bool drawError){    
   gROOT->ProcessLine("setTDRStyle();");
+
+  gStyle->SetHatchesSpacing(1);
+  gStyle->SetHatchesLineWidth(1);
 
   gStyle->SetPaintTextFormat("4.1f");
   gStyle->SetOptStat(0);
   gStyle->SetOptTitle(0);
   TGaxis::SetMaxDigits(3);
 
-  for(int iChannel=0; iChannel<nChannel; ++iChannel){
-    auto h_tmp = (TH1D *)h_stb->h_acceptance_deltaR[iChannel]->Clone();
-    h_tmp->Scale(100);
-    h_tmp->GetYaxis()->SetTitle("Acceptance(%)");
-    drawHist(h_tmp, true);
-    h_tmp->~TH1D();
+  gStyle->SetHatchesSpacing(1);
+  gStyle->SetHatchesLineWidth(1);
 
-    h_tmp = (TH1D *)h_stb->h_acceptance_invMass[iChannel]->Clone();
-    h_tmp->Scale(100);
-    h_tmp->GetYaxis()->SetTitle("Acceptance(%)");
-    drawHist(h_tmp, true);
-    h_tmp->~TH1D();
-      
-    h_tmp = (TH1D *)h_stb->h_purity_deltaR[iChannel]->Clone();
-    h_tmp->Scale(100);
-    h_tmp->GetYaxis()->SetTitle("Purity(%)");
-    drawHist(h_tmp, false);
-    h_tmp->~TH1D();
-    
-    h_tmp = (TH1D *)h_stb->h_purity_invMass[iChannel]->Clone();
-    h_tmp->Scale(100);
-    h_tmp->GetYaxis()->SetTitle("Purity(%)");
-    drawHist(h_tmp, false);
-    h_tmp->~TH1D();
-
-    h_tmp = (TH1D *)h_stb->h_stability_deltaR[iChannel]->Clone();
-    h_tmp->Scale(100);
-    h_tmp->GetYaxis()->SetTitle("Stability(%)");
-    drawHist(h_tmp, false);
-    h_tmp->~TH1D();
-    
-    h_tmp = (TH1D *)h_stb->h_stability_invMass[iChannel]->Clone();
-    h_tmp->Scale(100);
-    h_tmp->GetYaxis()->SetTitle("Stability(%)");
-    drawHist(h_tmp, false);
-    h_tmp->~TH1D();
-  }
-  f_outFile->Close();
-}
-
-void drawHist(TH1* h_in_, bool drawError){
   auto h_in = (TH1 *)h_in_->Clone();
+  h_in->Scale(100);
   auto h_err = (TH1 *)h_in->Clone();
   string histname = h_in->GetName();
 
   TCanvas *c = new TCanvas("","",800,800);
   TPaveText *label_sim = tdrCMSSimlabel();
+  TPaveText *label_work = tdrWorkProgress();
 
   ssize_t pos;
   h_in->SetMaximum(h_in->GetMaximum()*1.05);
   if((pos = histname.find("AcceptanceDeltaR")) != string::npos){
+    h_in->GetYaxis()->SetTitle("Acceptance(%)");
     h_in->SetMaximum(1.2);
     h_in->SetMinimum(0.6);
   }
   if((pos = histname.find("AcceptanceInvMass")) != string::npos){
+    h_in->GetYaxis()->SetTitle("Acceptance(%)");
     h_in->SetMaximum(2.0);
     h_in->SetMinimum(0.0);
   }
   if((pos = histname.find("Purity")) != string::npos){
+    h_in->GetYaxis()->SetTitle("Purity(%)");
     h_in->SetMaximum(70);
     h_in->SetMinimum(30);
   }
   if((pos = histname.find("Stability")) != string::npos){
+    h_in->GetYaxis()->SetTitle("Stability(%)");
     h_in->SetMaximum(70);
     h_in->SetMinimum(30);
   }
-  h_in->GetYaxis()->SetTitleOffset(1.5);
+  
+  h_in->GetYaxis()->SetTitleOffset(1.3);
   h_in->GetXaxis()->SetTitleOffset(1.2);
   h_in->SetLineWidth(2);
   h_in->SetLineColor(kBlue);
   h_in->Draw("hist");
   if(drawError){
-    h_err->SetFillStyle(3013);
-    h_err->SetFillColor(kBlue);
+    h_err->SetFillStyle(3154);
+    h_err->SetFillColor(kBlack);
     h_err->Draw("e2 same");
   }
 
-
+  label_work->Draw("same");
   label_sim->Draw("same");
-  c->Print(Form("%s/%s.pdf", output_pdf.c_str(), h_in->GetName()),"pdf");
+  c->Print(Form("%s/%s_%s.pdf", output_pdf.c_str(), h_in->GetName(), name.c_str()),"pdf");
 
   c->~TCanvas();
   h_in->~TH1();
