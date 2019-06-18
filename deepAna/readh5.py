c14bbd27f633b4db2186e7e27614365c13fc6059
TAE JEONG KIM <monttj@gmail.com>
2019-06-18 14:24:13 +0900

    add cuts

--- deepAna/readh5.py
+++ deepAna/readh5.py
@@ -12,9 +12,17 @@ parser.add_option("-f", "--inputFile", metavar='F', type='string', action='store
                   dest='inputFile',
                   help='Input file')
 
+parser.add_option("-c", "--cuts", metavar='F', type='string', action='store',
+                  default = "",
+                  dest='cuts',
+                  help='cuts')
+
 (options, args) = parser.parse_args()
 
 print options.inputFile
 
 df = pd.read_hdf(options.inputFile)
+df = df.query(options.cuts)
+
 print df
+
