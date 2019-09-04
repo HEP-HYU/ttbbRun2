75a5c9c676be58aef107e4ea4db11763e794d2a3
Somhammer <klar.wind0425@gmail.com>
2019-09-04 15:50:04 +0900

    make hist.yaml automatically

---
+++ macro/getHistList.py
@@ -0,0 +1,16 @@
+import os, sys
+from ROOT import *
+import ROOT
+
+f_data = ROOT.TFile("../output/root16/hist_DataSingleMu.root")
+hist_list = [x.GetName() for x in f_data.GetListOfKeys()]
+
+f_hist = open("../plotIt/configs/histos_ttlj.yml", 'w')
+
+for hist in hist_list:
+    f_hist.write("'"+hist+"':\n")
+    f_hist.write('  y-axis: "Events"\n')
+    f_hist.write('  y-axis-format: "%1% / %2$.2f"\n')
+    f_hist.write("  save-extensions: ['pdf']\n")
+    f_hist.write('  log-y: true\n')
+    f_hist.write('  show-ratio: true\n\n')
