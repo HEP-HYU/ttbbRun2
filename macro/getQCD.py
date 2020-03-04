7353f729442209b40fcabe3db51f55f4753285a0
Somhammer <klar.wind0425@gmail.com>
2020-03-04 17:30:03 +0900

    change location

--- macro/getQCD.py
+++ macro/getQCD.py
@@ -6,11 +6,11 @@ import sys
 def getQCDShape(base_path, year, sample_list):
     print("Get QCD shape in the sideband region")
     print("YEAR: "+str(year))
-    input_path = base_path+'/output/root'+str(year)+'/'
+    input_path = base_path+'/output/root'+str(year)+'_qcd/'
 
-    f_qcd = TFile.Open(os.path.join(input_path, "hist_dataDrivenQCD.root"), "recreate")
-    f_muon = TFile.Open(os.path.join(input_path, "hist_dataDrivenQCD_DataSingleMu.root"))
-    f_elec = TFile.Open(os.path.join(input_path, "hist_dataDrivenQCD_DataSingleEG.root"))
+    f_qcd = TFile.Open(os.path.join(input_path, "hist_dataDriven_QCD.root"), "recreate")
+    f_muon = TFile.Open(os.path.join(input_path, "hist_dataDriven_DataSingleMu.root"))
+    f_elec = TFile.Open(os.path.join(input_path, "hist_dataDriven_DataSingleEG.root"))
 
     hist_list = []
     for x in f_muon.GetListOfKeys():
@@ -38,7 +38,7 @@ def getQCDShape(base_path, year, sample_list):
 
     samples = {}
     for proc in sample_list:
-        f_tmp = TFile.Open(os.path.join(input_path, "hist_dataDrivenQCD_"+proc+".root"))
+        f_tmp = TFile.Open(os.path.join(input_path, "hist_dataDriven_"+proc+".root"))
         samples[proc] = f_tmp
 
     f_qcd.cd()
@@ -57,16 +57,18 @@ def getQCDShape(base_path, year, sample_list):
 
         for proc in sample_list:
             if 'QCD' in proc: continue
+            #print("Sample: "+str(proc))
             h_tmp = samples[proc].Get(hist)
             scale = lumi[year]*xsec[proc]/genevt[proc]
             h_tmp.Scale(scale)
             h_qcd.Add(h_tmp,-1)
 
         if "TransverseMass" in hist:
-            if "S3" in hist: continue
             nevt_qcd = h_qcd.Integral(0, h_qcd.GetNbinsX()+1)
             print("Purity of QCD")
             print("Hist: "+str(hist))
+            print("nevt_data: "+str(nevt_data))
+            print("nevt_qcd: "+str(nevt_qcd))
             purity = float(nevt_qcd)/float(nevt_data)
             print("Puriy: "+str(purity))
 
