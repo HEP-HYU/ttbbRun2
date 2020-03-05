2b70632735d4cf859971b4c4f2ad645451e514b9
Somhammer <klar.wind0425@gmail.com>
2020-03-05 16:04:05 +0900

    rm duplication

--- macro/drawCutflowTable.py
+++ macro/drawCutflowTable.py
@@ -8,11 +8,12 @@ luminosities = {16:35922, 17:41529, 18:59693}
 
 for year in range(16,19):
     tmp_tmp = 0.0
-    
+    input_loc = '../output/root'+str(year)+'_post/'
+
     f_nevt = open('../output/cutflow/nevt'+str(year)+'.tex', 'w')
     f_config = open('../plotIt/configs/files'+str(year)+'.yml','w')
 
-    f_config.write("'root"+str(year)+"/hist_DataSingleMu.root':\n")
+    f_config.write(input_loc+"/hist_DataSingleMu.root':\n")
     f_config.write("  type: data\n")
     f_config.write("  legend: 'Data'\n")
     f_config.write("  pretty-name: 'DataMu'\n")
@@ -20,7 +21,7 @@ for year in range(16,19):
     f_config.write("  group: GData\n")
     f_config.write("\n")
 
-    f_config.write("'root"+str(year)+"/hist_DataSingleEG.root':\n")
+    f_config.write(input_loc+"/hist_DataSingleEG.root':\n")
     f_config.write("  type: data\n")
     f_config.write("  legend: 'Data'\n")
     f_config.write("  pretty-name: 'DataEG'\n")
@@ -75,7 +76,7 @@ for year in range(16,19):
 		elif "#d0cfd4" in color: group = 'GQCD'
 		else: group = ''
 
-		f_config.write("'root"+str(year)+"/hist_"+sample+".root':\n")
+		f_config.write(input_loc+"/hist_"+sample+".root':\n")
 		f_config.write("  type: mc\n")
 		f_config.write("  pretty-name: '"+sample+"'\n")
 		f_config.write("  cross-section: "+str(xsec)+"\n")
@@ -87,7 +88,7 @@ for year in range(16,19):
 		#f_config.write("  scale: "+str(sf[sample])+"\n")
 		f_config.write("\n")
 
-		f_sample = ROOT.TFile('../output/root'+str(year)+'/hist_'+sample+'.root')
+		f_sample = ROOT.TFile(input_loc+'/hist_'+sample+'.root')
 		scale = (luminosities[year]*xsec/genevt[sample])
 		print(sample+": "+str(scale))
 		nevt = sample + " "
@@ -96,15 +97,15 @@ for year in range(16,19):
                         hist_tmp = f_sample.Get(hist_name+'_Ch'+str(ich)+'_S'+str(istep))
                         hist_tmp.Scale(scale)
                         tmp_evt = hist_tmp.Integral(0, hist_tmp.GetNbinsX()+1)
-                        tmp_stat = hist_tmp.IntegralAndError(0, hist_tmp.GetNbinsX()+1, ROOT.Double(tmp_tmp));
-                        nevt += '& $%.2f {\scriptstyle\ \pm\ %.2f}$ ' % (tmp_evt, tmp_tmp)
+                        tmp_stat = hist_tmp.IntegralAndError(0, hist_tmp.GetNbinsX()+1, ROOT.Doubl(tmp_tmp));
+                        nevt += '& $%.2f {\scriptstyle\ \pm\ %.2f}$ ' % (tmp_evt, tmp_stat)
 		nevt += '\\ \n'
 		f_nevt.write(nevt)
             n += 1
 
     # Data
-    f_muon = ROOT.TFile('../output/root'+str(year)+'/hist_DataSingleMu.root')
-    f_elec = ROOT.TFile('../output/root'+str(year)+'/hist_DataSingleEG.root')
+    f_muon = ROOT.TFile(input_loc+'/hist_DataSingleMu.root')
+    f_elec = ROOT.TFile(input_loc+'/hist_DataSingleEG.root')
     nevt = 'Data'
     for ich in range(0,2):
         for istep in range(0,4):
@@ -117,7 +118,7 @@ for year in range(16,19):
                 tmp_evt = hist_elec.Integral()+hist_elec.GetBinContent(hist_elec.GetNbinsX()+1)
                 tmp_stat = hist_elec.IntegralAndError(0, hist_elec.GetNbinsX()+1, ROOT.Double(tmp_tmp))
 
-            nevt += '& $%.2f {\scriptstyle\ \pm\ %.2f}$ ' % (tmp_evt, tmp_tmp)
+            nevt += '& $%.2f {\scriptstyle\ \pm\ %.2f}$ ' % (tmp_evt, tmp_stat)
     nevt += '\n'
     f_nevt.write(nevt)
     f_nevt.write('        \\hline\n')
@@ -127,14 +128,3 @@ for year in range(16,19):
 
     f_nevt.close()
     f_config.close()
-
-scale16 = float(35922)/float(137144)
-scale17 = float(41529)/float(137144)
-scale18 = float(59693)/float(137144)
-os.system('cp ../plotIt/configs/files16.yml ../plotIt/configs/files16_copy.yml\n')
-os.system('cp ../plotIt/configs/files17.yml ../plotIt/configs/files17_copy.yml\n')
-os.system('cp ../plotIt/configs/files18.yml ../plotIt/configs/files18_copy.yml\n')
-os.system('sed -i "s/scale: 1/scale: '+str(scale16)+'/g" ../plotIt/configs/files16_copy.yml\n')
-os.system('sed -i "s/scale: 1/scale: '+str(scale17)+'/g" ../plotIt/configs/files17_copy.yml\n')
-os.system('sed -i "s/scale: 1/scale: '+str(scale18)+'/g" ../plotIt/configs/files18_copy.yml\n')
-os.system('cat ../plotIt/configs/files16_copy.yml ../plotIt/configs/files17_copy.yml ../plotIt/configs/files18_copy.yml > ../plotIt/configs/files.yml\n')
